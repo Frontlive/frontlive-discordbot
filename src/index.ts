61d9bd3527b245977da35084c1f233bf4a638be7
Szymon Hrabia <35381167+Survikrowa@users.noreply.github.com>
2022-01-26 18:15:03 +0100

    fix/remove-unused-intents

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,8 @@ const client = new SapphireClient({
 	shards: 'auto',
 	intents: [
 		'GUILDS',
-		'GUILD_MEMBERS',
-		'GUILD_BANS',
-		'GUILD_EMOJIS_AND_STICKERS',
-		'GUILD_VOICE_STATES',
 		'GUILD_MESSAGES',
-		'GUILD_MESSAGE_REACTIONS',
 		'DIRECT_MESSAGES',
-		'DIRECT_MESSAGE_REACTIONS'
 	]
 });
 
