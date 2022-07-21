f87f4a9f6eddfbfda2c451e4bc29e69b8a59e0b0
survikrowa <szymonhrabia60@gmail.com>
2022-07-21 17:33:42 +0200

    feat/add-info-message-on-dzieki-response

--- src/utils/handleCommandUserErrors.ts
+++ src/utils/handleCommandUserErrors.ts
@@ -4,7 +4,7 @@ export const handleCommandUserErrors = (errorIdentifier: string) => {
       return 'Oznacz użytkownika jako pierwszy argument.';
     }
     case 'argsMissing': {
-      return 'Nie podałeś argumentów! Jako pierwszy arugment oznacz użytkownika, któremu dziękujesz, a jako drugi argument jeżeli chcesz, wiadomość za co mu dziekujesz.';
+      return 'Nie podałeś argumentów! Jako pierwszy argument oznacz użytkownika, któremu dziękujesz, a jako drugi argument jeżeli chcesz, wiadomość za co mu dziekujesz.';
     }
     default: {
       return 'Coś poszło nie tak! Oznacz admina Survi i go uderz w głowę.';
