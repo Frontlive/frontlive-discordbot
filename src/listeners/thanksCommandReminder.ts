64df6029b247487bfb32ca1b48feea117f9e90d0
Szymon Hrabia <35381167+Survikrowa@users.noreply.github.com>
2022-07-21 17:52:02 +0200

    Feat/improve dzieki (#12)
    * feat/improve-dzieki
    
    * feat/improve-dzieki
    
    * feat/improve-dzieki

--- src/listeners/thanksCommandReminder.ts
+++ src/listeners/thanksCommandReminder.ts
@@ -2,6 +2,7 @@ import { Listener, SapphireClient } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
 const TECHNOLOGY_CHANNEL_ID = '878973710382530601';
+const BOT_CHANNEL_ID = '943116275494838303';
 
 export class ThanksCommandReminder extends Listener {
   private readonly client: SapphireClient;
@@ -20,7 +21,7 @@ export class ThanksCommandReminder extends Listener {
       const channel = await this.client.channels.fetch(message.channelId);
       //@ts-ignore another shitty type from sapphire creators
       const isChannelParentCategoryTechnology = channel.parentId === TECHNOLOGY_CHANNEL_ID;
-      if (isChannelParentCategoryTechnology) {
+      if (isChannelParentCategoryTechnology || message.channelId === BOT_CHANNEL_ID) {
         message.channel.send(
           `<@${message.author.id}> Nie zapomnij podziękować za pomocą !dzieki. Jako pierwszy argument oznacz użytkownika, któremu dziękujesz, a jako drugi jeżeli chcesz, wiadomość za co mu dziekujesz`,
         );
